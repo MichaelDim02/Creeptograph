4503eb271d48301ee968ce6314c8a854ffdacf5e
Michael Constantine Dimopoulos <31562759+MichaelDim02@users.noreply.github.com>
2018-01-05 10:13:33 +0000

    Update creeptograph.py
    Made it so it uses both input and raw_input depending on the python version

--- creeptograph.py
+++ creeptograph.py
@@ -2,9 +2,18 @@
 from __future__ import print_function
 import string
 import sys
+#Function so this script works for both versions 2 and 3 
+def in_put(text):
+	if (sys.version_info.major >= 3):
+		import base64 
+		inp = input(str(text))
+		return inp
+	else:
+		inp = raw_input(str(text))
+		return inp
 def decryption():
 	print("Enter text")
-	text = raw_input("> ")
+	text = in_put("> ")
 	print("\n")
 	en = {'g':'a', 'a':'b', 's':'c', '4':'d', '2':'e','3':'f','m':'g', 'c':'h', 'f':'i', 'r':'j', 'q':'k', '1':'l', 'd':'m', '9':'n', '0':'o', 't':'p', 'b':'q', 'n':'r', 'x':'s', 'j':'t', 'z':'u', 'p':'v', '8':'w', '7':'x', 'v':'y', '5':'z', " ": " ", ".":".", ",":",", "!":"!", "?":"?"}
 	for letter in text:
@@ -12,7 +21,7 @@ def decryption():
 	print("\n")
 def encryption():
 	print("Enter text")
-	text = raw_input("> ")
+	text = in_put("> ")
 	print("\n")
 	en = {'a':'g', 'b':'a', 'c':'s', 'd':'4', 'e':'2','f':'3','g':'m', 'h':'c', 'i':'f', 'j':'r', 'k':'q', 'l':'1', 'm':'d', 'n':'9', 'o':'0', 'p':'t', 'q':'b', 'r':'n', 's':'x', 't':'j', 'u':'z', 'v':'p', 'w':'8', 'x':'7', 'y':'v', 'z':'5', " ": " ", ".":".", ",":",", "!":"!", "?":"?"}
 	for letter in text:
@@ -24,7 +33,7 @@ def banner():
 	print("		1. Encrypt")
 	print("		2. Decrupt")
 	print("		3. Exit")
-	opt = raw_input("> ")
+	opt = in_put("> ")
 	if opt == "1":
 		encryption()
 	elif opt == "2":
